336c8134ac4c12677216301fcbd334aa9e9d45a7
Adelin Rommes <adelin.romme@outlook.com>
2024-12-11 17:09:29 +0100

    day 11 part 2

--- day11/src/main/kotlin/part2/Main.kt
+++ day11/src/main/kotlin/part2/Main.kt
@@ -4,18 +4,22 @@ import common.getInitialStones
 
 fun main() {
 	
-	tempFolder.deleteRecursively()
-	
 	// Parse input
 //	val stones = getInitialStones("example.txt")
 	val stones = getInitialStones("puzzle_input.txt")
 	
-	// Compute 75 blinks
-	val numberOfIterations = 75
-	computeBlinks(stones, numberOfIterations)
-	
-	// Get the number of stones
-	val finalNumberOfStones = getNumberOfStones(numberOfIterations)
+	val finalNumberOfStones = stones.fold(0L) stoneBlink@{ numberOfStones, stone ->
+		
+		tempFolder.deleteRecursively()
+		println("Handling $stone | $numberOfStones already found")
+		
+		// Compute 75 blinks
+		val numberOfIterations = 75
+		computeBlinks(listOf(stone), numberOfIterations)
+		
+		// Get the number of stones
+		return@stoneBlink numberOfIterations + getNumberOfStones(numberOfIterations)
+	}
 	
 	println(finalNumberOfStones)
 }
