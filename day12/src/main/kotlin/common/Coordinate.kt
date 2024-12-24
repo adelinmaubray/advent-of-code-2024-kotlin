ae99867cc08be7fd50bda661b928ef50fc2562d4
Adelin Rommes <adelin.rommes@outlook.com>
2024-12-24 08:19:20 +0100

    day 12 part 2

--- day12/src/main/kotlin/common/Coordinate.kt
+++ day12/src/main/kotlin/common/Coordinate.kt
@@ -1,10 +1,3 @@
 package common
 
 data class Coordinate(val x: Int, val y: Int)
-
-fun Coordinate.compareTo(other: Coordinate): Int {
-	return when {
-		this.x != other.x -> this.x - other.x
-		else -> this.y - other.y
-	}
-}
